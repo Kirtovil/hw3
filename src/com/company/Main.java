63de641b60bb735217da132846ca42c19e01dc14
Alexandr Polikarpov <nub.and.co@gmail.com>
2022-01-04 23:51:57 +0300

    ДЗ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,69 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
 
+        int clientDeviceYear = 2015;
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("установите версию приложения для Android по ссылке");
+        }
+
+        int year = 2654;
+        if (year % 4 == 0 || year % 400 == 0) {
+            System.out.println("год является високосным");
+        } else {
+            System.out.println("год не является високосным");
+        }
+
+        int deliveryDistance = 95;
+        int deliveryDay = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("потербуется дней - " + deliveryDay);
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            System.out.println("потребуется дней - " + ++deliveryDay);
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("потребуется дней - " + (++deliveryDay + 1));
+        }
+
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("осень");
+                break;
+            default:
+                break;
+        }
     }
 }
